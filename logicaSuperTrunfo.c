872fbbbed0110f3b68f4e07d5064054e85bbef27
brendinhaADS-25 <brenda07eduarda@hotmail.com>
2025-12-12 18:40:14 +0000

    commit3

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -13,7 +13,7 @@ int main() {
     char estado, codigo [10], cidade [10];   
     float pessoas, pib, area;     
     int pontos; 
-    
+    int carta1 = 0; 
     // Cadastro das Cartas:
     // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
     // utilizando a função scanf para capturar as entradas.
@@ -45,6 +45,7 @@ int main() {
     char Estado, Cidade [10], Codigo [10]; 
     float Pessoas, Pib, Area; 
     int Ponto; 
+    int carta2 = 0; 
 
     printf("Uma letra de 'A', 'B', (representando um estado):\n" ); 
     scanf(" %c", &Estado); 
@@ -73,27 +74,40 @@ int main() {
 
     if(area > Area){
        printf("A Carta1 tem maior área\n"); 
+       carta1++; 
     } else{
       printf("A Carta2 tem maior área\n"); 
+      carta2++; 
     }
 
     if(pessoas > Pessoas){
        printf("A Carta1 tem a maior população\n"); 
+       carta1++; 
     } else{
       printf("A Carta2 tem a maoir população\n"); 
+      carta2++; 
     }
 
     if(pib > Pib){
        printf("A Carta1 tem maior PIB\n"); 
+       carta1++; 
     } else{
       printf("A Carta2 tem maior PIB\n"); 
+      carta2++; 
     }
-
     
     if(pontos > Ponto){
-       printf("A Carta1 tem maior número de pontos turísticos\n");  
+       printf("A Carta1 tem maior número de pontos turísticos\n"); 
+       carta1++;  
     } else{
       printf("A Carta2 tem maior número de pontos turísticos\n"); 
+      carta2++; 
+    } 
+
+    if(carta1 > carta2){
+       printf("A CARTA1 É A VENCEDORA!!!\n"); 
+    } else{
+      printf("A CARTA2 É A VENCEDORA!!!\n"); 
     }
     // Exemplo:
     // if (populacaoA > populacaoB) {
@@ -108,6 +122,7 @@ int main() {
 
     // Exemplo:
     // printf("A cidade vencedora é: %s\n", cidadeVencedora);
+       printf("A cidade vencedora é: %s\n "); 
 
     return 0;
 }
